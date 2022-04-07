cc5512d94de9733840f3ae8c1795c895c01045fd
“John1114” <john_rytel@brown.edu>
2022-04-06 21:17:08 -0400

    minor fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ const Header = (props) => {
       <p className='header2style' style={{fontSize: '50px', opacity: '0.5'}}>The Brown Entrepreneurship Conference</p>
       <div className="horizontalLine"></div>
       <div className="speakersHeader" style={{height: props.height}}>
-        <p className="info1" style={{fontSize: '70px', paddingTop: '70px', margin: 'auto', marginBottom:'50px'}}>{props.title}</p>
-        <p className="info2" style={{fontSize: '30px', inlineSize:'800px', margin: 'auto'}}>{props.caption}</p>
+        <p className="Hinfo1">{props.title}</p>
+        <p className="Hinfo2">{props.caption}</p>
       </div>
     </div>
   )
